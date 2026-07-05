2e367bcad90a64559e5cc803aa4513800dfd4a03
Ziah <zvanblerck1@gmail.com>
2026-07-05 12:59:23 +0200

    added knock off piano tiles
    - crop audio
    - need to make a lose screen kinda vibe
    -make it bigger

--- out-of-place/Scripts/camera_pivot.gd
+++ out-of-place/Scripts/camera_pivot.gd
@@ -1,28 +1,40 @@
 extends Node3D
 
-#bob_speed = 6–10 for slower or faster movement.
-#bob_amount = 0.03–0.10 for a subtle or stronger effect.
+@export var cameraSens = 0.004
+@export var cameraSpeed = 0.1
 
-#need to add smooth transitions when moving
+@export var maxZoom = 40
+@export var minZoom = 8
 
-@export var bob_speed := 8.0
-@export var bob_amount := 0.08
+@onready var camera_3d: Camera3D = $Camera3D
 
-var time := 0.0
-var original_position: Vector3
+func _ready() -> void:
+	#Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	pass
 
-func _ready():
-	original_position = position
+func _input(_event):
+#below does the camera invisble
+	#if event is InputEventMouseMotion:
+		#rotation.y -= event.relative.x * cameraSens
+	_camera_zoom()
+##use mouse wheel to zoom in or out
+func _camera_zoom():
+	var zoomChange = 0
+	if Input.is_action_pressed("mouse_wheel_up"):
+		zoomChange -= 1
+	elif Input.is_action_pressed("mouse_wheel_down"):
+		zoomChange += 1
+	
+	camera_3d.size += zoomChange
+	camera_3d.size = clamp(camera_3d.size, minZoom, maxZoom)
 
-func _process(delta):
-	var player = get_parent()
+func _physics_process(_delta: float) -> void:
+	_camera_movement()
 
-	# Only bob when moving horizontally
-	var moving = Vector2(player.velocity.x, player.velocity.z).length() > 0.1
-
-	if moving:
-		time += delta * bob_speed
-		position.y = original_position.y + sin(time) * bob_amount
-	else:
-		time = 0.0
-		position.y = lerp(position.y, original_position.y, delta * 10)
+##WASD to move camera
+func _camera_movement():
+	var direction = Vector2.ZERO
+	direction.y = Input.get_axis("camera_up", "camera_down")
+	direction.x = Input.get_axis("camera_left", "camera_right")
+	
+	global_position += (global_basis * Vector3(direction.x, 0, direction.y)).normalized() * cameraSpeed
